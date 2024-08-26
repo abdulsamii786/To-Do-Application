124583bbe0e58effe366e36efbc92847fbdf6978
Abdul Sami <154473245+abdulsamii786@users.noreply.github.com>
2024-08-27 01:15:37 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
-let inputField = document.querySelector("#input");
+let todoData = [];
 let titleField = document.querySelector("#title");
-let paraBox = document.querySelector("#para");
-let titleBox = document.querySelector("#title-box");
-let gettingDate = document.querySelector("#date");
+let todoBox = document.querySelector(".show-todos-box");
 let error = document.querySelector("#error");
+let updateBtn = document.getElementById("update");
+let addBtn = document.querySelector(".add");
+updateBtn.style.display = "none";
+let id;
 
-function getInput() {
-  if (inputField.value === "" || titleField.value === "") {
-    error.innerText = "Title and Task should not be empty!";
+function addTodo() {
+  if (titleField.value.trim() === "") {
+    error.innerText = "Invalid Input";
+    setTimeout(() => {
+      error.innerText = "";
+    }, 2000);
   } else {
-    error.innerText = "";
-    let d = new Date();
-    paraBox.innerText += `${inputField.value} \n`;
-    inputField.value = "";
-    titleBox.innerText += titleField.value + "\n";
+    todoData.push(titleField.value);
+    showTodo();
     titleField.value = "";
-    gettingDate.innerText += d.toLocaleString() + "\n";
   }
 }
+
+titleField.addEventListener("keyup", (e) => {
+  if (e.key === "Enter" && !addBtn.disabled) {
+    addTodo();
+  }
+});
+
+function showTodo() {
+  todoBox.innerHTML = "";
+  for (let i = 0; i < todoData.length; i++) {
+    todoBox.innerHTML += `<div class="todos">${todoData[i]}
+      <div class="btn-wrapper">
+      <button class="delete" onclick="deleteTodo(${i})"><i class="fa fa-trash" aria-hidden="true"></i></button>
+      <button class="edit" onclick="editTodo(${i})"><i class="fas fa-edit"></i></button></div>
+      </div>`;
+  }
+}
+
+function deleteTodo(index) {
+  todoData.splice(index, 1);
+  showTodo();
+}
+
+function editTodo(index) {
+  updateBtn.style.display = "block";
+  addBtn.setAttribute("disabled", "disabled");
+  addBtn.style.backgroundColor = "#999";
+  titleField.value = todoData[index];
+  id = index;
+  showTodo();
+}
+
+function updateTodo(id) {
+  updateBtn.style.display = "none";
+  addBtn.removeAttribute("disabled", "disabled");
+  addBtn.style.backgroundColor = "#6f42c1";
+  todoData.splice(id, 1, titleField.value);
+  showTodo();
+
+  titleField.value = "";
+}
+
+updateBtn.addEventListener("click", () => {
+  updateTodo(id);
+});
 
 let gettingYear = document.querySelector("#year");
 
